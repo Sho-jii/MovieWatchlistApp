b5797b15f2fe7f1fefe467797a15240d7b8e7c98
Sho-jii <jarib.sioco2004s@gmail.com>
2026-09-14 01:56:43 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.moviewatchlist',
+  appName: 'MovieWatchlistApp',
+  webDir: 'dist'
+};
+
+export default config;
